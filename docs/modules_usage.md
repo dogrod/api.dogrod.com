0930865100fc0871fbdf9a0aceba23a72b370b32
dogrod <rodrickjue@hotmail.com>
2017-09-03 17:15:41 +0800

    feat: add env config support by dotenv

---
+++ docs/modules_usage.md
@@ -0,0 +1,5 @@
+dependencies:
+- dotenv: 从 .env 文件中读取环境配置信息存储于 process.env 中
+
+devDependencies:
+- ts-node: typescript 运行环境
--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,26 @@
+import * as dotenv from 'dotenv'
 import * as express from 'express'
 
+/**
+ * Load environment variables from .env file, where API keys and passwords are configured.
+ */
+dotenv.config({ path: '.env' })
+
+/**
+ * Create Express server.
+ */
 const app = express()
 
+/**
+ * Express configuration.
+ */
+app.set('port', process.env.PORT || 3000)
+
+/**
+ * Primary app routes.
+ */
 app.get('/', (req: express.Request, res: express.Response) => {
   res.send('hello express')
 })
 
-app.listen(3000)
+app.listen(app.get('port'))
