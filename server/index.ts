88d4fb9ee7cfeb1e8ba4007c0531c8b460242346
dogrod <rodrickjue@hotmail.com>
2017-09-03 17:21:50 +0800

    feat: render home page via pug template

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,15 @@
 import * as dotenv from 'dotenv'
 import * as express from 'express'
-
+import * as path from 'path'
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
 dotenv.config({ path: '.env' })
+
+/**
+ * Controllers (route handlers).
+ */
+import * as homeController from './controllers/home'
 
 /**
  * Create Express server.
@@ -15,12 +20,12 @@ const app = express()
  * Express configuration.
  */
 app.set('port', process.env.PORT || 3000)
+app.set('views', path.join(process.cwd(), 'client/views'))
+app.set('view engine', 'pug')
 
 /**
  * Primary app routes.
  */
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.send('hello express')
-})
+app.get('/', homeController.index)
 
 app.listen(app.get('port'))
